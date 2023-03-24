6962c391d90b4397554cf6307cc2d712c95ce4c5
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-03-24 14:11:07 +0100

    Added some docstrings.

---
+++ profiling_datasets.py
@@ -0,0 +1,29 @@
+"""This script is used to run the profiling operation on all datasets in the 
+given data_folder with an engine passed as argument. 
+"""
+
+import pandas as pd 
+import polars as pl
+import json
+from pathlib import Path
+from src.utils import profile_dataset
+from tqdm import tqdm
+import sys
+
+# Setting working folder
+working_folder = Path(".")
+root_data_folder = Path(working_folder, "data")
+data_folder = Path(root_data_folder, "soda-data-lake/a-d3m/a-d3m_full")
+assert data_folder.exists()
+
+dataset_list = list(data_folder.iterdir())
+
+df_info_overall = pd.DataFrame()
+
+for dataset_path in tqdm(dataset_list, total=len(dataset_list)):
+    #TODO: Something explodes with a-d3m_full on 174, possibly running out of 
+    #TODO: memory. 
+    df_info = profile_dataset(dataset_path=dataset_path, engine=sys.argv[1])
+    df_info_overall = pd.concat([df_info_overall, df_info])
+
+df_info_overall.to_csv("info_a-d3m_full.csv", index=False)
